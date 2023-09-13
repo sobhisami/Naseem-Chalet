e4131bef8c69ccc78fa2fb7f91234eab91459a17
sobhisami <sobhisami073@gmail.com>
2023-09-13 15:36:17 +0300

    fix subtitle

--- src/Components/SubTitle/SubTitle.jsx
+++ src/Components/SubTitle/SubTitle.jsx
@@ -2,11 +2,7 @@ import './style.css'
 const SubTitle = ({children,variant}) => {
   return (
     <span className={variant==="sub-title-w"?"sub-title-w animate-text":"sub-title animate-text"}>
-        <svg width="100%" height="100%">
-        <text x="50%" y="60%"  text-anchor="middle"  >
-        {children} 
-        </text>
-        </svg>
+        {children}
       </span>
   )
 }
