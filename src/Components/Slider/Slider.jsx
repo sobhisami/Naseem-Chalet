efc0b8ca7ea44182613b84a8eb4d60fee26bd2bc
sobhisami <sobhisami073@gmail.com>
2023-09-25 10:17:43 +0300

    slider edit image

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -4,15 +4,15 @@ import "react-image-gallery/styles/css/image-gallery.css";
 const images = [
   {
     original: "../../images/frame.png",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    thumbnail: "../../images/frame.png",
   },
   {
     original: "../../images/crai-center.png",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    thumbnail: "../../images/crai-center.png",
   },
   {
     original: "../../images/crai-right.png",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    thumbnail: "../../images/crai-right.png",
   },
 ];
 const Slider = () => {
